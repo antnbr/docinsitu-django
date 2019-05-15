d9dc783b77542fa2d19dacf3a147e700c228d4a7
antnbr <antoine.bntes@gmail.com>
2019-05-15 18:12:51 +0200

    Added views to create/edit articles inside the site

--- render/urls.py
+++ render/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.render_view, name='render_view'),
     path('article/<int:pk>', views.article_detail, name='article_detail'),
+    path('article/new/', views.article_new, name='article_new'),
+    path('article/<int:pk>/edit/', views.article_edit, name='article_edit'),
 ]
--- render/views.py
+++ render/views.py
@@ -1,12 +1,40 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Article
+from .forms import ArticleForm
 
 # initial view
 def render_view(request):
-    articles = Article.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    articles = Article.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'render/render_view.html', {'articles': articles})
 
 def article_detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
     return render(request, 'render/article_detail.html', {'article': article})
+
+def article_new(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.published_date = timezone.now()
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm()
+    return render(request, 'render/article_edit.html', {'form': form})
+
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.published_date = timezone.now()
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'render/article_edit.html', {'form': form})
